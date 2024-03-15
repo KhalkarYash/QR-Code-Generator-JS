ea9bdc4d63e8fdbe12e6360afee9b85dd953ea08
KhalkarYash <yashmk2004@gmail.com>
2024-03-15 23:37:00 +0530

    Add QR code generation endpoint and frontend

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-import inquirer from "inquirer";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import qr from "qr-image";
 import fs from "fs";
 
-inquirer
-  .prompt([{ message: "Type in your URL: ", name: "URL" }])
-  .then((answers) => {
-    console.log(answers);
-    const url = answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_image.png"));
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.post("/generate-qr", (req, res) => {
+    const { url } = req.body;
+    if (!url) return res.status(400).json({ error: "URL is required" });
+
+    const qrPath = `public/qr_code.png`;
+    var qr_svg = qr.image(url, { type: "png" });
+    qr_svg.pipe(fs.createWriteStream(qrPath));
+
+    res.json({ qrPath: "qr_code.png" });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
